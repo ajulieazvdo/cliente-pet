27062b3cea260e0a781cb6143e6726e55d071d5e
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-16 14:51:16 -0300

    implementa controller e fluxo para service

---
+++ src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteService.java
@@ -0,0 +1,10 @@
+package br.com.petz.clientepetz.cliente.application.service;
+
+import br.com.petz.clientepetz.cliente.application.api.ClienteRequest;
+import br.com.petz.clientepetz.cliente.application.api.ClienteResponse;
+
+public interface ClienteService {
+
+	ClienteResponse criaCliente(ClienteRequest clienteRequest);
+
+}
