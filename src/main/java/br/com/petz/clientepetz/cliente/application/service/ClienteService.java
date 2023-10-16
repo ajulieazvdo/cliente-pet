cda8dc5f89b04ae9f7d53d1997db9a843a5c5418
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-16 15:37:58 -0300

    implementa ClienteApplicationService e corrige ClienteAPI

--- src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteService.java
@@ -4,7 +4,5 @@ import br.com.petz.clientepetz.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepetz.cliente.application.api.ClienteResponse;
 
 public interface ClienteService {
-
 	ClienteResponse criaCliente(ClienteRequest clienteRequest);
-
 }
