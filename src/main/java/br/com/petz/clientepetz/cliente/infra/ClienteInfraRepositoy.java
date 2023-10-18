217372b9a77829f3dbf247793d8055a72144d7f9
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-18 16:35:52 -0300

    implementa fluxo do repository. Teste ok

---
+++ src/main/java/br/com/petz/clientepetz/cliente/infra/ClienteInfraRepositoy.java
@@ -0,0 +1,19 @@
+package br.com.petz.clientepetz.cliente.infra;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.petz.clientepetz.cliente.application.repository.ClienteRepository;
+import br.com.petz.clientepetz.cliente.domain.Cliente;
+import lombok.extern.log4j.Log4j2;
+@Repository
+@Log4j2
+public class ClienteInfraRepositoy implements ClienteRepository {
+
+	@Override
+	public Cliente salva(Cliente cliente) {
+		log.info("[inicia] ClienteInfraRepository - salva");
+		log.info("[finaliza] ClienteInfraRepository - salva");
+		return cliente;
+	}
+
+}
