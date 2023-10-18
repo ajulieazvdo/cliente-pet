b1c85c1aba4d8d8ad12fe677a7e783fb91a07447
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-18 17:30:55 -0300

    implementa fluxo service. Teste ok

--- src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteService.java
@@ -1,8 +1,12 @@
 package br.com.petz.clientepetz.cliente.application.service;
 
+import java.util.List;
+
+import br.com.petz.clientepetz.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepetz.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepetz.cliente.application.api.ClienteResponse;
 
 public interface ClienteService {
 	ClienteResponse criaCliente(ClienteRequest clienteRequest);
+	List<ClienteListResponse> buscaTodosClientes();
 }
