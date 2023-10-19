1d2eb5c5d49e76a9e7622ac4ed30fb5194effd62
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-19 15:30:54 -0300

    implementa fluxo de alteracao camada api

--- src/main/java/br/com/petz/clientepetz/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepetz/cliente/application/api/ClienteAPI.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,13 +22,20 @@ public interface ClienteAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
+	
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
 	List<ClienteListResponse> getTodosClientes();
+
 	@GetMapping(value = "/{idCliente}")
 	@ResponseStatus(code = HttpStatus.OK)
 	ClienteDetalhadoResponse getClienteAtravesId(@PathVariable UUID idCliente);
+	
 	@DeleteMapping(value = "/{idCliente}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	void deletaClienteAtravesId(@PathVariable UUID idCliente);
+	
+	@PatchMapping(value = "/{idCliente}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void patchAlteraCliente(@PathVariable UUID idCliente, @Valid @RequestBody ClienteAlteracaoRequest clienteAlteracaoRequest);
 }
---
+++ src/main/java/br/com/petz/clientepetz/cliente/application/api/ClienteAlteracaoRequest.java
@@ -0,0 +1,24 @@
+package br.com.petz.clientepetz.cliente.application.api;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.petz.clientepetz.cliente.domain.Sexo;
+import lombok.Value;
+@Value
+public class ClienteAlteracaoRequest {
+	@NotBlank
+	private String nomeCompleto;
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@NotNull
+	private Boolean aceitaTermos;
+}
