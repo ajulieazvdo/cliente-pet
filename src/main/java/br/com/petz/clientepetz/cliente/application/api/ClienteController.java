f5fa006f2871da8ff7c9431cdc1ddb831d1af212
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-19 08:41:09 -0300

    implementa api get cliente atraves do id

--- src/main/java/br/com/petz/clientepetz/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepetz/cliente/application/api/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.petz.clientepetz.cliente.application.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,9 +27,17 @@ public class ClienteController implements ClienteAPI {
 	@Override
 	public List<ClienteListResponse> getTodosClientes() {
 		log.info("[inicia] ClienteController - getTodosClientes");
-		log.info("[finaliza] ClienteController - getTodosClientes");
 		List<ClienteListResponse> clientes = clienteService.buscaTodosClientes();
+		log.info("[finaliza] ClienteController - getTodosClientes");
 		return clientes;
 	}
 
+	@Override
+	public ClienteDetalhadoResponse getClienteAtracesId(UUID idCliente) {
+		log.info("[inicia] ClienteController - getClienteAtracesId");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finaliza] ClienteController - getClienteAtracesId");		
+		return null;
+	}
+	
 }
---
+++ src/main/java/br/com/petz/clientepetz/cliente/application/api/ClienteDetalhadoResponse.java
@@ -0,0 +1,20 @@
+package br.com.petz.clientepetz.cliente.application.api;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.petz.clientepetz.cliente.domain.Sexo;
+import lombok.Value;
+@Value
+public class ClienteDetalhadoResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String email;
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	private Boolean aceitaTermos;
+	private LocalDateTime dataHoraDoCadastro;
+
+}
