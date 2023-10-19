dec09bfdc5398eb53b098e5495eaff3410c14bbb
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-19 16:08:29 -0300

    implementa alteracao

--- src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteApplicationService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.petz.clientepetz.cliente.application.api.ClienteAlteracaoRequest;
 import br.com.petz.clientepetz.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.clientepetz.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepetz.cliente.application.api.ClienteRequest;
@@ -52,6 +53,15 @@ public class ClienteApplicationService implements ClienteService {
 		clienteRepository.deletaCliente(cliente);
 		log.info("[finaliza] ClienteApplicationService - deletaClienteAtravesId");
 		
+	}
+
+	@Override
+	public void patchAlteraCliente(UUID idCliente, ClienteAlteracaoRequest clienteAlteracaoRequest) {
+		log.info("[inicia] ClienteApplicationService - patchAlteraCliente");
+		Cliente cliente = clienteRepository.buscaClienteAtravesId(idCliente);
+		cliente.altera(clienteAlteracaoRequest);
+		clienteRepository.salva(cliente);
+		log.info("[finaliza] ClienteApplicationService - patchAlteraCliente");		
 	}
 
 }
