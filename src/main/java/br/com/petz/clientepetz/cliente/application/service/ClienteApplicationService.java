4a9b037626b09942309a0393f92dcd643c2578ef
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-19 14:35:17 -0300

    implementa fluxo service delete

--- src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepetz/cliente/application/service/ClienteApplicationService.java
@@ -45,4 +45,11 @@ public class ClienteApplicationService implements ClienteService {
 		return new ClienteDetalhadoResponse(cliente);
 	}
 
+	@Override
+	public void deletaClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteApplicationService - deletaClienteAtravesId");
+		log.info("[finaliza] ClienteApplicationService - deletaClienteAtravesId");
+		
+	}
+
 }
