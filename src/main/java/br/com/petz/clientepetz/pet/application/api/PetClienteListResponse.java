cf80b0e6ce030dc913d8133fb90ddcbfedaae79b
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-20 16:29:16 -0300

    implementa conversao de pet

--- src/main/java/br/com/petz/clientepetz/pet/application/api/PetClienteListResponse.java
+++ src/main/java/br/com/petz/clientepetz/pet/application/api/PetClienteListResponse.java
@@ -1,13 +1,35 @@
 package br.com.petz.clientepetz.pet.application.api;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.petz.clientepetz.pet.domain.Pet;
-
+import br.com.petz.clientepetz.pet.domain.TipoPet;
+import lombok.Value;
+@Value
 public class PetClienteListResponse {
-
+	private UUID idPet;
+	private String nomePet;
+	private TipoPet tipoPet;
+	private String raca;
+	private String pelagemCor;
+	private LocalDate dataNascimento;
+	
 	public static List<PetClienteListResponse> converte(List<Pet> petsDoCliente) {
-		return null;
+		return petsDoCliente.stream()
+				.map(PetClienteListResponse::new)
+				.collect(Collectors.toList());
 	}
 
+	public PetClienteListResponse(Pet pet) {
+		this.idPet = pet.getIdPet();
+		this.nomePet = pet.getNomePet();
+		this.tipoPet = pet.getTipoPet();
+		this.raca = pet.getRaca();
+		this.pelagemCor = pet.getPelagemCor();
+		this.dataNascimento = pet.getDataNascimento();
+	}
+	
 }
