3309eea4a6e207512ca4b62c1ced5c4ff2e4005c
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-20 15:58:30 -0300

    implementa fluxo da api

--- src/main/java/br/com/petz/clientepetz/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepetz/pet/application/api/PetController.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepetz.pet.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
 import javax.validation.Valid;
@@ -23,6 +24,14 @@ public class PetController implements PetAPI {
 		PetResponse pet = petService.criaPet(idCliente, petRequest);
 		log.info("[finaliza] PetController - postPet");
 		return pet;
+	}
+
+	@Override
+	public List<PetClienteListResponse> getPetsDoClienteComId(UUID idCliente) {
+		log.info("[inicia] PetController - getPetsDoClienteComId");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finish] PetController - getPetsDoClienteComId");
+		return null;
 	}
 
 }
