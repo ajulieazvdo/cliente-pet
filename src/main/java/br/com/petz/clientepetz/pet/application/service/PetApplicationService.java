b07686a05bf86ad006bc8c2433988f0257390a61
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-20 14:44:48 -0300

    implementa fluxo service

---
+++ src/main/java/br/com/petz/clientepetz/pet/application/service/PetApplicationService.java
@@ -0,0 +1,24 @@
+package br.com.petz.clientepetz.pet.application.service;
+
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Service;
+
+import br.com.petz.clientepetz.pet.application.api.PetRequest;
+import br.com.petz.clientepetz.pet.application.api.PetResponse;
+import lombok.extern.log4j.Log4j2;
+@Service
+@Log4j2
+public class PetApplicationService implements PetService {
+
+	@Override
+	public PetResponse criaPet(UUID idCliente, @Valid PetRequest petRequest) {
+		log.info("[start] PetApplicationService - criaPet");
+		
+		log.info("[finish] PetApplicationService - criaPet");
+		return null;
+	}
+
+}
