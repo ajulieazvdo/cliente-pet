f75972a0554384a41e1266590b843b3bb545bb20
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-10-20 20:46:31 -0300

    implementa fluxo da camada de API

--- src/main/java/br/com/petz/clientepetz/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepetz/pet/application/api/PetController.java
@@ -35,4 +35,12 @@ public class PetController implements PetAPI {
 		return petsDoCliente;
 	}
 
+	@Override
+	public PetClienteDetalheResponse getPetDoClienteComId(UUID idCliente, UUID idPet) {
+		log.info("[inicia] PetController - getPetDoClienteComId");
+		log.info("[idCliente] {} - [idPet] {}", idCliente, idPet);		
+		log.info("[inicia] PetController - getPetDoClienteComId");
+		return null;
+	}
+
 }
